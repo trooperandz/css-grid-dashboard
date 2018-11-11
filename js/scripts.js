2af06d79ff176bddea3a9f2e7d05263fad49ae45
Matt Holland <mtholland10@gmail.com>
2018-11-11 17:40:56 -0600

    Update tiles with column-counts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* Scripts for css grid dashboard */
 
 function toggleClass(el, className) {
   if (el.hasClass(className)) {
@@ -42,55 +43,69 @@ function setSidenavListeners() {
 
 // Draw the chart
 function renderChart() {
-  const ctx = document.getElementById('chart-finance').getContext('2d');
-
-  // Set fixed height for chart
-  ctx.canvas.parentNode.style.height = '200px';
-
-  const financeChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-          datasets: [{
-              label: 'Spending per month',
-              data: [1150, 1000, 1675, 2100, 1340, 1500, 2350, 1835, 1350, 1900],
-              backgroundColor: [
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-                  'rgba(27,186,225,0.2)',
-              ],
-              borderColor: [
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-                  'rgba(27,186,225)',
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          },
-          maintainAspectRatio: false,
-      }
+  const chart = AmCharts.makeChart( "chartdiv", {
+    "type": "serial",
+    "theme": "light",
+    "dataProvider": [ {
+      "month": "Jan",
+      "visits": 2025
+    }, {
+      "month": "Feb",
+      "visits": 1882
+    }, {
+      "month": "Mar",
+      "visits": 1809
+    }, {
+      "month": "Apr",
+      "visits": 1322
+    }, {
+      "month": "May",
+      "visits": 1122
+    }, {
+      "month": "Jun",
+      "visits": 1114
+    }, {
+      "month": "Jul",
+      "visits": 984
+    }, {
+      "month": "Aug",
+      "visits": 711
+    }, {
+      "month": "Sept",
+      "visits": 665
+    }, {
+      "month": "Oct",
+      "visits": 580
+    } ],
+    "valueAxes": [ {
+      "gridColor": "#FFFFFF",
+      "gridAlpha": 0.2,
+      "dashLength": 0
+    } ],
+    "gridAboveGraphs": true,
+    "startDuration": 1,
+    "graphs": [ {
+      "balloonText": "[[category]]: <b>[[value]]</b>",
+      "fillAlphas": 0.8,
+      "lineAlpha": 0.2,
+      "type": "column",
+      "valueField": "visits"
+    } ],
+    "chartCursor": {
+      "categoryBalloonEnabled": false,
+      "cursorAlpha": 0,
+      "zoomable": false
+    },
+    "categoryField": "month",
+    "categoryAxis": {
+      "gridPosition": "start",
+      "gridAlpha": 0,
+      "tickPosition": "start",
+      "tickLength": 20
+    },
+    "export": {
+      "enabled": false
+    }
   });
 }
 
