6a6d8a56e32627fc1594f5f5a2adb49ecfa20394
Matt Holland <mtholland10@gmail.com>
2018-11-12 00:42:25 -0600

    Refine cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 /* Scripts for css grid dashboard */
 
+// Set constants and grab needed elements
+const sidenavEl = $('.sidenav');
+const gridEl = $('.grid');
+const SIDENAV_ACTIVE_CLASS = 'sidenav--active';
+const GRID_NO_SCROLL_CLASS = 'grid--noscroll';
+
 function toggleClass(el, className) {
   if (el.hasClass(className)) {
     el.removeClass(className);
@@ -18,7 +24,7 @@ function setUserDropdownListener() {
   });
 }
 
-// Side nav list sliding functionality
+// Sidenav list sliding functionality
 function setSidenavListeners() {
   const subHeadings = $('.navList__subheading'); console.log('subHeadings: ', subHeadings);
   const SUBHEADING_OPEN_CLASS = 'navList__subheading--open';
@@ -109,8 +115,48 @@ function renderChart() {
   });
 }
 
+function toggleClass(el, className) {
+  if (el.hasClass(className)) {
+    el.removeClass(className);
+  } else {
+    el.addClass(className);
+  }
+}
+
+// If user opens the menu and then expands the viewport from mobile size without closing the menu,
+// make sure scrolling is enabled again and that sidenav active class is removed
+function addResizeListeners() {
+  $(window).resize(function(e) {
+    const width = window.innerWidth; console.log('width: ', width);
+
+    if (width > 750) {
+      sidenavEl.removeClass(SIDENAV_ACTIVE_CLASS);
+      gridEl.removeClass(GRID_NO_SCROLL_CLASS);
+    }
+  });
+}
+
+// Menu open sidenav icon, shown only on mobile
+function setMenuClickListener() {
+  $('.header__menu').on('click', function(e) { console.log('clicked menu icon');
+    toggleClass(sidenavEl, SIDENAV_ACTIVE_CLASS);
+    toggleClass(gridEl, GRID_NO_SCROLL_CLASS);
+  });
+}
+
+// Sidenav close icon
+function setSidenavCloseListener() {
+  $('.sidenav__brand-close').on('click', function(e) {
+    toggleClass(sidenavEl, SIDENAV_ACTIVE_CLASS);
+    toggleClass(gridEl, GRID_NO_SCROLL_CLASS);
+  });
+}
+
 $(document).ready(() => {
+  addResizeListeners();
   setSidenavListeners();
   setUserDropdownListener();
   renderChart();
+  setMenuClickListener();
+  setSidenavCloseListener();
 });
